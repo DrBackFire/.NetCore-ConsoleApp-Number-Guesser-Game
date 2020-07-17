d01a3133e6c8e77c1e4905ae8882aff5b23b024e
DrBackFire <54931900+DrBackFire@users.noreply.github.com>
2020-07-17 15:54:07 +1000

    Split classes into own files

---
+++ number guesser/Form.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace number_guesser
+{
+    class Form
+    {
+        public virtual void Lables(string lable)
+        {
+            Console.WriteLine("Enter Your " + lable);
+        }
+
+        public string UserName()
+        {
+            Lables("Full Name:");
+            string Name = Console.ReadLine();
+
+            // Checking User Input
+            while (string.IsNullOrEmpty(Name))
+            {
+
+                Alerts(ConsoleColor.Red, "Please Enter Your Name");
+
+                // Reset text color
+                Console.ResetColor();
+
+                Console.WriteLine("Name can't be empty! Input your name once more");
+                Lables("Full Name:");
+                Name = Console.ReadLine();
+
+            }
+
+            return Name;
+        }
+
+        public int Age()
+        {
+            // Asking for user input
+            Console.WriteLine("Enter the year you where born at");
+
+            int year;
+
+            // Getting user input
+            string stringYear = Console.ReadLine();
+
+            // Validating user input
+            while (!int.TryParse(stringYear, out year))
+            {
+                Alerts(ConsoleColor.Red, "Please Enter a Number");
+
+                // Reset text color
+                Console.ResetColor();
+
+                Console.WriteLine("Enter the year you where born at");
+
+                stringYear = Console.ReadLine();
+
+                bool parseYear = int.TryParse(stringYear, out year);
+
+                if (parseYear)
+                {
+                    continue;
+                }
+
+            }
+
+            int userYear = Convert.ToInt32(stringYear);
+
+            var age = 2020 - userYear;
+
+            return age;
+        }
+
+        // Setting alerts for errors
+        public void Alerts(ConsoleColor color, string messege)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(messege);
+        }
+
+    }
+
+}
---
+++ number guesser/NumberGame.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace number_guesser
+{
+    class NumberGame : Form // Inheriting methods from Form
+    {
+        // Overriding inherted method
+        public override void Lables(string lable)
+        {
+            Console.WriteLine(lable);
+        }
+
+        public void Game()
+        {
+
+            while (true)
+            {
+                // Create a new Random object
+                Random random = new Random();
+
+                Console.WriteLine("Choose a number between 1 and 10");
+
+                // Init correct number
+                int correctNumber = random.Next(1, 10);
+
+                // Init guess var
+                int guess = 0;
+
+                Lables("Enter Your Number:");
+
+
+                while (guess != correctNumber)
+                {
+                    // Asking user for an input
+                    var Userinput = Console.ReadLine();
+
+                    // Validate user input
+                    if (!int.TryParse(Userinput, out guess))
+                    {
+                        // Alerting User
+                        Alerts(ConsoleColor.Red, "Enter a number!");
+
+                        // Reset text color
+                        Console.ResetColor();
+
+                        // Keep Going
+                        continue;
+                    }
+
+                    // Converting User input to a number
+                    guess = Convert.ToInt32(Userinput);
+
+                    // Validate answer
+                    if (guess != correctNumber)
+                    {
+                        Alerts(ConsoleColor.DarkGreen, "Wrong! Try again");
+
+                        // Reset text color
+                        Console.ResetColor();
+
+                        Lables("Enter Your Number:");
+                    }
+                    else
+                    {
+                        Alerts(ConsoleColor.Blue, "Correct!");
+
+                        Console.ResetColor();
+
+                        // Ask to play again
+                        Lables("Play Again? [Y or N]");
+
+                        // Get answer
+                        string answer = Console.ReadLine().ToUpper();
+
+                        if (answer == "Y")
+                        {
+                            continue;
+                        }
+                        else if (answer == "N")
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            return;
+                        }
+
+
+                    }
+                }
+
+
+
+
+            }
+        }
+    }
+}
